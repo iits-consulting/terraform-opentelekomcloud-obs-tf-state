9a143f27d662d3f1a8281de4c9ffbe717186ec22
canaykin <70644420+canaykin@users.noreply.github.com>
2021-07-27 16:11:49 +0200

    Encryption policy can now be disabled
    An optional variable force_encryption is added to give the module user the ability to disable the encryption policy on the bucket to allow unencrypted writes the the bucket along with encrypted state.

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
 variable "bucket_name" {
   description = "The bucket name where the encrypted tf file should be stored"
 }
+variable "force_encryption" {
+  default = true
+  description = "Add an encryption enforcing policy to the state bucket. Default = true"
+}
 variable "region" {}
