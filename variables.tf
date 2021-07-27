b7dd1670775e21e47060e80451036a302bbbcdea
canaykin <70644420+canaykin@users.noreply.github.com>
2021-07-27 16:17:06 +0200

    bugfix-linting

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "bucket_name" {
   description = "The bucket name where the encrypted tf file should be stored"
 }
 variable "force_encryption" {
-  default = true
+  default     = true
   description = "Add an encryption enforcing policy to the state bucket. Default = true"
 }
 variable "region" {}
